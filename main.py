90501474f73cb5d6b0e168a4ec1e212a835a1869
Pati Mikulska <pp.mikulska@gmail.com>
2017-04-22 14:52:40 +0200

    Geting json

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -8,6 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/receive', methods=["POST"])
+def receive():
+    pick = request.get_json()
+    print(pick)
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
