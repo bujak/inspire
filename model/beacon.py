696104e4ae8ec9c8a4011608e71b7b96d49b883e
Tomek Bujakowski <bujakowski@gmail.com>
2017-04-22 17:08:55 +0200

    Add add_pick method in beacon model

--- model/beacon.py
+++ model/beacon.py
@@ -1,13 +1,25 @@
+import model.sql as sql
+
 class Beacon():
     def __init__(self, id, pick):
         self.id = id
         self.pick = pick
 
+    @staticmethod
+    def add_pick(beacon_id, user_id):
+        query = """SELECT * FROM picks WHERE beacon_id = (?)"""
+        param = [beacon_id]
 
-    def add_pick(self):
-        self.pick += 1
+        if not sql.query(query, param):
+            query = """INSERT INTO picks (user_id, beacon_id, amount) VALUES (?, ?, ?)"""
+            values = [user_id, beacon_id, 1]
+            sql.query(query,values)
+        else:
+            query = """UPDATE picks SET amount = amount + 1 WHERE beacon_id =(?)"""
+            sql.query(query, param)
+        return
 
     @staticmethod
     def get_by_id(beacon_id):
-        #TODO get from DB
+        query = """SELECT """
         return
